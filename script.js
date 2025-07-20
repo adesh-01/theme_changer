7c5902e9c4f8f3e17417a0e01f6ec1f3252d563f
adesh-01 <147783069+adesh-01@users.noreply.github.com>
2025-07-20 10:36:58 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+
+const buttons = document.querySelectorAll('.button')
+const body = document.querySelector('body')
+
+buttons.forEach( (btn) => {
+    console.log(btn);
+    btn.addEventListener('click',function (evnt){
+        console.log(evnt)
+        console.log(evnt.target);
+        if(evnt.target.id === "grey"){
+            body.style.backgroundColor = evnt.target.id
+        }
+        if(evnt.target.id === "white"){
+            body.style.backgroundColor = evnt.target.id
+        }
+        if(evnt.target.id === "blue"){
+            body.style.backgroundColor = evnt.target.id
+        }
+        if(evnt.target.id === "yellow"){
+            body.style.backgroundColor = evnt.target.id
+        }
+    })
+})
+
